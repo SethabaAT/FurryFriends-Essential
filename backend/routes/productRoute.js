93df015537b50da12a8d8f7c1592d19152d5b868
Arnold Thabo Sethaba <atsethaba@gmail.com>
2023-09-13 20:53:51 +0200

    No need to be an admin to get product, removed that from all the get products functions

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -12,12 +12,10 @@ import { isAdmin } from "../middleware/authorization.js";
 // Set End points
 const router = express.Router();
 router.route("/addProduct").post(isAdmin, addProduct);
-router.route("/getProductById/:id").get(isAdmin, getProductByID);
-router.route("/getAllProducts").get(isAdmin, getAllProducts);
 router.route("/removeProduct/:id").delete(isAdmin, removeProduct);
 router.route("/updateProduct/:id").put(isAdmin, updateProduct);
-router
-  .route("/getProductByCategory/:category")
-  .get(isAdmin, getProductByCategory);
+router.route("/getProductById/:id").get(getProductByID);
+router.route("/getAllProducts").get(getAllProducts);
+router.route("/getProductByCategory/:category").get(getProductByCategory);
 
 export default router;
