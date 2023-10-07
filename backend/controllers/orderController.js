756a9e2fffe68b7ec0d1cccd268d30e3a7e8daa9
Arnold Thabo Sethaba <atsethaba@gmail.com>
2023-10-07 15:55:27 +0200

    Created the invoice in the database, functions and endpoints

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,12 +2,7 @@ import Order from "../models/Order.js";
 import crypto from "crypto";
 import Product from "../models/Product.js";
 import Cart from "../models/Cart.js";
-
-// Generate the order id since I will use it immediately after creation
-const generateOrderId = () => {
-  const hash = crypto.randomBytes(16).toString("hex");
-  return hash;
-};
+import Invoice from "../models/Invoice.js";
 
 // A function for adding order to the database
 export const createOrder = async (req, res, next) => {
@@ -25,30 +20,20 @@ export const createOrder = async (req, res, next) => {
 
     // Add order items to the order we just created'
     console.log(data);
-    for (const { product_id, qty } of data) {
-      await order.addOrderItem(order_id, product_id, qty); // Note that this id here is product id
-
-      // Get the product by id and update the quantity
-      let product = await Product.findById(product_id);
-      total += product.discount * qty;
-
-      // Update the product qty in the databse
-      let updatedProduct = new Product(
-        product.name,
-        product.category_id,
-        product.description,
-        product.price,
-        product.qty - qty,
-        product.image,
-        product.discount
-      );
-      await Product.update(product_id, updatedProduct);
-    }
+    await addOrderItems(order, order_id, total, data);
 
     // After adding all the order items, update the total
     await Order.updateTotal(order_id, total);
     await Cart.deleteAllItems(user_id);
-    res.status(201).json({ message: "Order Succesfully Added" });
+    const invoiceDetails = await createInvoice(
+      order_id,
+      "invoices/invoice.pdf",
+      data
+    );
+
+    res
+      .status(201)
+      .json({ message: "Order Succesfully Added", invoice: invoiceDetails });
   } catch (error) {
     console.log("Error in adding the order", error);
     next(error);
@@ -92,5 +77,110 @@ export const getOrderItems = async (req, res, next) => {
   } catch (error) {
     console.log("Error in getting order items", error);
     next(error);
+  }
+};
+
+// get invoices
+export const getInvoices = async (req, res, next) => {
+  try {
+    const user_id = req.user_id;
+    const invoices = await Invoice.findUserInvoices(user_id);
+    res.status(200).json(invoices);
+  } catch (error) {
+    console.log("Error in getting invoices", error);
+    next(error);
+  }
+};
+
+// Get invoice
+export const getInvoice = async (req, res, next) => {
+  try {
+    const [invoice, _] = await Invoice.findById(req.params.id);
+
+    // If the invoice does not exist
+    if (!invoice) {
+      return res.status(404).json({ message: "Invoice not found" });
+    }
+
+    console.log(invoice.order_id);
+    // Get lines from the database
+    let lines = await Invoice.getInvoiceLines(invoice.order_id);
+    console.log("lines", lines);
+    // Get invoice datails for each product
+    let invoice_details = [];
+
+    for (const { name, qty, price } of lines) {
+      var invoice_line = {
+        product_name: name,
+        product_qty: qty,
+        product_price: price,
+        total: qty * price,
+      };
+      invoice_details.push(invoice_line);
+    }
+
+    res.status(200).json(invoice_details);
+  } catch (error) {
+    console.log("Error in getting invoice", error);
+    next(error);
+  }
+};
+
+// Utility functions
+// Generate the order id since I will use it immediately after creation
+const generateOrderId = () => {
+  const hash = crypto.randomBytes(16).toString("hex");
+  return hash;
+};
+
+// Add order items
+const addOrderItems = async (order, order_id, total, data) => {
+  console.log(data);
+  for (const { product_id, qty } of data) {
+    await order.addOrderItem(order_id, product_id, qty); // Note that this id here is product id
+
+    // Get the product by id and update the quantity
+    let product = await Product.findById(product_id);
+    total += product.discount * qty;
+
+    // Update the product qty in the databse
+    let updatedProduct = new Product(
+      product.name,
+      product.category_id,
+      product.description,
+      product.price,
+      product.qty - qty,
+      product.image,
+      product.discount
+    );
+    await Product.update(product_id, updatedProduct);
+  }
+};
+
+// Create Invoice
+const createInvoice = async (order_id, invoice_location, data) => {
+  try {
+    const invoice = new Invoice(order_id, invoice_location);
+    await invoice.save();
+
+    // Get invoice datails for each product
+    let invoice_details = [];
+
+    for (const { product_id, qty } of data) {
+      let product = await Product.findById(product_id);
+
+      var invoice_line = {
+        product_name: product.name,
+        product_qty: qty,
+        product_price: product.price,
+        total: qty * product.price,
+      };
+      invoice_details.push(invoice_line);
+    }
+
+    return invoice_details;
+  } catch (error) {
+    console.log("Error in adding invoice", error);
+    return null;
   }
 };
--- frontend/react-furry/src/pages/admin/admin.jsx
+++ frontend/react-furry/src/pages/admin/admin.jsx
@@ -1,11 +1,12 @@
-import { Link, useNavigate } from 'react-router-dom'
-import { Button } from '../../components/button'
-import {React, useContext} from 'react'
-import { ShopContext } from '../../context/shop-context'
-
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "../../components/button";
+import { React, useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+import { PowerBIEmbed } from "powerbi-client-react";
+import { models } from "powerbi-client";
+import "./admin.css";
 
 export const Admin = () => {
-    
   const { userType, token } = useContext(ShopContext);
   return (
     <>
@@ -14,17 +15,61 @@ export const Admin = () => {
           <h4> UnAuthorised Access, Please Sign In. </h4>
         </div>
       ) : (
-    <div className='admin'>
-        {/* <Button text={"Add Product"} onClickAdd={() => navigate('/addProducts')}/>
+        <div className="admin">
+          {/* <Button text={"Add Product"} onClickAdd={() => navigate('/addProducts')}/>
         <Button text={"Remove Product"} onClickAdd={() => navigate('/removeProducts')}/>
         <Button text={"Update Product"} onClickAdd={() => navigate('/updateProducts')}/> */}
-      <h4>Admin tasks</h4>
-        <ul className='admin-tasks'>
-          <li><Link to={'/addProducts'}>Add Product(s)</Link></li>
-          <li><Link to={'/removeProducts'}>Remove Product(s)</Link></li>
-          <li><Link to={'/updateProducts'}>Update Product(s)</Link></li>
-        </ul>
-
-    </div> )} </>
-  )
-}
+          <h4>Admin Dashboard</h4>
+          <PowerBIEmbed
+            embedConfig={{
+              type: "report", // Supported types: report, dashboard, tile, visual, qna, paginated report and create
+              id: "85b26cf3-e303-44dc-95bd-0641ebf0c8a2",
+              embedUrl:
+                "https://app.powerbi.com/reportEmbed?reportId=85b26cf3-e303-44dc-95bd-0641ebf0c8a2&groupId=47799f1e-15cb-4d76-98dd-a88a18e9a27e&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLU5PUlRILUVVUk9QRS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldCIsImVtYmVkRmVhdHVyZXMiOnsidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d",
+              accessToken:
+                "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZDhiZjdjMTgtNTcyNS00YjllLWIxMTgtMTMzODhmNTJlNDRlLyIsImlhdCI6MTY5NjY3ODMxMywibmJmIjoxNjk2Njc4MzEzLCJleHAiOjE2OTY2ODM0MTAsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVlFBcS84VUFBQUE3b2I0dmh6YTdXSjlXbGlXZ1BweXFzK0NDQ1BSejZSMGxhczVxQVY5VU5YVFY2MC9vZkRUVXBLK2p5MjFsbS9EM2t0Vy9raTRzRlZYYXFtRDY1eWxwdHZTVDJ1bWdId2xzd0xiWG1saTlqND0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcGlkIjoiODcxYzAxMGYtNWU2MS00ZmIxLTgzYWMtOTg2MTBhN2U5MTEwIiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJNQUNISSIsImdpdmVuX25hbWUiOiJOVEFORE9ZRU5LT1NJIiwiaXBhZGRyIjoiMTUyLjEwNi45OS4xNDgiLCJuYW1lIjoiTlRBTkRPWUVOS09TSSBNQUNISSIsIm9pZCI6ImM5MTYzODRmLWFhOTAtNGU5NS04ZDJmLWU4ZjE4MzBmNmMzOSIsIm9ucHJlbV9zaWQiOiJTLTEtNS0yMS0xMDY3MTI0NjQ3LTEzMzgyMjM4MjMtMTY4MTc5OTE3Mi01MTY5NzciLCJwdWlkIjoiMTAwMzIwMDFEMkFBOEIwOSIsInJoIjoiMC5BVG9BR0h5XzJDVlhua3V4R0JNNGoxTGtUZ2tBQUFBQUFBQUF3QUFBQUFBQUFBQTZBUGcuIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic3ViIjoiN0M2TjlUeWlyeGtHdmlNRlU1V202cDRsR1l5MzUtbFNobUNuVWJEZFdvTSIsInRpZCI6ImQ4YmY3YzE4LTU3MjUtNGI5ZS1iMTE4LTEzMzg4ZjUyZTQ0ZSIsInVuaXF1ZV9uYW1lIjoiMjIyMTAxODU0QHN0dWRlbnQudWouYWMuemEiLCJ1cG4iOiIyMjIxMDE4NTRAc3R1ZGVudC51ai5hYy56YSIsInV0aSI6InZRcmRfZGhScTBpQzZWWUZhR3BvQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdfQ.jkU7J7ikGmVQ_g3duizqkLTRSjwwO3S6M5RIlcPEsAJb7mC1PrXKUqAzrM3B_W9FYJ1SXppSN-F676O5xOU3J9eJ4NOx1CXPtYigd7fzzQgUxQNtdP8JKnj1-osjsLUsNh7epzRXyL8M6L5gwMWM_BfhzwrSQexseFZiGoBGcbMi2usog5bTRyM2VJoukWWhCKvBPK5YBTVuyDzf8WG8OfpQVoQdW9VFR1jexUQLcUz_MwA0J4z6PjIirrQwhPtSFVGP6gWpq0mfsCHdEa9I-r55KNYGrOUq-zulJNbFYbKosVPZMgZAsN_aY6cUtzSVCWVbMBp1UrtczgFaSfZAcA",
+              tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
+              settings: {
+                panes: {
+                  filters: {
+                    expanded: false,
+                    visible: true,
+                  },
+                },
+                //background: models.BackgroundType.Transparent,
+              },
+            }}
+            eventHandlers={
+              new Map([
+                [
+                  "loaded",
+                  function () {
+                    console.log("Report loaded");
+                  },
+                ],
+                [
+                  "rendered",
+                  function () {
+                    console.log("Report rendered");
+                  },
+                ],
+                [
+                  "error",
+                  function (event) {
+                    console.log(event.detail);
+                  },
+                ],
+                ["visualClicked", () => console.log("visual clicked")],
+                ["pageChanged", (event) => console.log(event)],
+              ])
+            }
+            cssClassName={"Embed-container"}
+            getEmbeddedComponent={(embeddedReport) => {
+              window.report = embeddedReport;
+            }}
+          />
+        </div>
+      )}
+    </>
+  );
+};
