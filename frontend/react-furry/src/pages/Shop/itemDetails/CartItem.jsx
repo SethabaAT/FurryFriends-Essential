703bec26cb234cf97e56874f2afc9dc5cd436e31
Tahb0 <97204337+Tahb0@users.noreply.github.com>
2023-10-09 07:20:26 +0200

    add to cart

--- frontend/react-furry/src/pages/Shop/itemDetails/CartItem.jsx
+++ frontend/react-furry/src/pages/Shop/itemDetails/CartItem.jsx
@@ -1,6 +1,6 @@
 import { React, useContext } from "react";
 import { ShopContext } from "../../../context/shop-context";
-import "./Cart.css";
+
 import {
   addToCart,
   removeFromCart,
@@ -8,24 +8,19 @@ import {
   subtractFromCart,
 } from "../../../Service/service";
 
-
 export const CartItem = (props) => {
   const { qty } = props.pqty;
   const { id, name, price, image, discount } = props.data;
-  const {
-    cartState,
-    toggleCartState,
-    token,    
-  } = useContext(ShopContext);
+  const { cartState, toggleCartState, token } = useContext(ShopContext);
 
   const handleRemove = async (id) => {
     try {
-        const res = await removeFromCart(id, token);
-        toggleCartState();
-        console.log(res);
-      } catch (er) {
-        console.error("coulndt add to cart", er);
-      }
+      const res = await removeFromCart(id, token);
+      toggleCartState();
+      console.log(res);
+    } catch (er) {
+      console.error("coulndt add to cart", er);
+    }
   };
 
   const handleAddToCart = async (id) => {
@@ -48,46 +43,66 @@ export const CartItem = (props) => {
     }
   };
 
-  const updateCartItemCount = async (id,quantity) => {
+  const updateCartItemCount = async (id, quantity) => {
     try {
-        const res = await updateProduct(id,parseInt(quantity), token);
-        toggleCartState();
-        console.log(res);
-      } catch (er) {
-        console.error("coulndt add to cart", er);
-      }
-
+      const res = await updateProduct(id, parseInt(quantity), token);
+      toggleCartState();
+      console.log(res);
+    } catch (er) {
+      console.error("coulndt add to cart", er);
+    }
   };
 
   return (
     <>
-    <tr>
-        <td><img src={image} width={40} height={40} className="cartproduct-image" /></td>
-              <td>{name}</td>
-              <td><button className="delete-from-cart" onClick={() => handleRemove(id)}>Remove</button></td>
-              <td><div className="handleItemCount">
-         <button className="updateCart" onClick={() => handleSubtract(id)}>
-           -
-         </button>
-         <input
-           className="cart-inpt"
-           value={props.pqty}
-           min={1}
-           type="number"
-           onChange={(e) => updateCartItemCount(id,e.target.value)}
-         />
-         <button className="updateCart" onClick={() => handleAddToCart(id)}>
-           +
-         </button>
-       </div></td>
-              <td>   R {price > discount ? discount : price}
-           {price > discount ? (
-             <span style={{fontSize:12}}>
-               <del> R {price}</del>
-             </span>
-           ) : null}</td>
-              <td>R {(price * props.pqty).toFixed(2)}</td>
-            </tr>
+      <tr>
+        <td>
+          <img
+            src={image}
+            width={60}
+            height={60}
+            className="cartproduct-image"
+          />
+        </td>
+        <td>{name}</td>
+        <td>
+          <button className="delete-from-cart" onClick={() => handleRemove(id)}>
+            Remove
+          </button>
+        </td>
+        <td>
+          <div  className="qtyInput">
+            <button className="updateCart" onClick={() => handleSubtract(id)}>
+              -
+            </button>
+            <label>
+            <input
+             
+              value={props.pqty}
+              min={"1"}
+              type="number"
+              onChange={(e) =>
+                updateCartItemCount(id, parseInt(e.target.value))
+              }
+             
+            />
+             </label>
+            <button className="updateCart" onClick={() => handleAddToCart(id)}>
+              +
+            </button>
+          </div>
+        </td>
+        <td>
+          {" "}
+          R {price > discount ? discount : price}
+          {price > discount ? (
+            <span style={{ fontSize: 12 }}>
+              <del> R {price}</del>
+            </span>
+          ) : null}
+        </td>
+        <td>R {(price * props.pqty).toFixed(2)}</td>
+      </tr>
     </>
   );
 };
